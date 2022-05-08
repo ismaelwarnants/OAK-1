1b16df1ea5b087330f1271c43d5a1cefc01e19fb
ismaelwarnants <ismael.warnants@gmail.com>
2022-05-08 23:27:35 +0200

    Added testing state

--- Testproject/depthai_blazepose-main/fall_detection.py
+++ Testproject/depthai_blazepose-main/fall_detection.py
@@ -9,60 +9,79 @@ sys.path.append("../..")
 from mediapipe_utils import KEYPOINT_DICT
 
 margin_of_error_on_angle = 10 #degrees
-
-#body_position_array = []
+testing = False
+max_length_of_array = 30
+body_position_array = []
 
 def detect_fall(body):
-    def angle_with_y(v):
+    def angle_with_x(v):
         # v: 2d vector (x,y)
-        # Returns angle in degree of v with y-axis of image plane
+        # Returns angle in degree of v with x-axis of image plane
 
         # v[0] = x = is verschil in breedte
         # v[1] = y = is verschil in hoogte
-        if v[1] == 0:
-            return 90
-        angle = atan2(v[0], v[1])
+        if v[0] == 0:
+            return 0
+        angle = atan2(v[1], v[0])
         return degrees(angle)
 
-    '''def add_angle_to_list(angle):
-        if len(body_position_array) >= 30:
-            body_position_array.remove(0)
-        body_position_array.append([time.time_ns(),angle])'''
+    def add_angle_to_list(angle):
+        #print("Length: "+ str(len(body_position_array)))
+        if len(body_position_array) >= max_length_of_array:
+            try:
+                body_position_array.pop(0)
+            except:
+                print("List is empty")
+        body_position_array.append([time.time_ns(),angle])
 
     def person_is_on_the_ground(angle):
         return -margin_of_error_on_angle < angle < margin_of_error_on_angle
 
     def person_has_fallen(angle):
-        #add_angle_to_list(angle)
+        if not testing:
+            add_angle_to_list(angle)
 
         timestamps = []
         angles = []
-        '''for position in body_position_array:
-            timestamps = timestamps + [position[0]]
-            angles = angles + [position[1]]'''
-        return person_is_on_the_ground(angle)#(max(angles) - min(angles)) >= 45 and max(timestamps) - min(timestamps) >= 500000000 and person_is_on_the_ground(angle)
 
-    upper_body_angle_with_y = 0
-    lower_body_angle_with_y = 0
+        if testing:
+            return person_is_on_the_ground(angle)
+        else:
+            for position in body_position_array:
+                timestamps.append(position[0])
+                angles.append(position[1])
+            angle_diff = (max(angles) - min(angles))
+            time_diff = max(timestamps) - min(timestamps)
+            fall = person_is_on_the_ground(angle)
+            print("Angle: " + str(angle_diff) + "degrees \t Time: " + str(time_diff/1000000) + "ms\t Fall: "+ str(fall))
+            print("Angles: \n"+str(angles))
+            print("Timestamps: \n"+str(timestamps))
+            #return (max(angles) - min(angles)) >= 70 and max(timestamps) - min(timestamps) >= 500000000 and person_is_on_the_ground(angle)
+            return angle_diff>=45 and time_diff>= 500000000 and fall
+
+    upper_body_angle_with_x = 0
+    lower_body_angle_with_x = 0
 
     try:
-        upper_body_angle_with_y = angle_with_y(body.landmarks[KEYPOINT_DICT['nose'],:2] - (body.landmarks[KEYPOINT_DICT['left_hip'],:2]+body.landmarks[KEYPOINT_DICT['right_hip'],:2])/2)
+        upper_body_angle_with_x = angle_with_x(body.landmarks[KEYPOINT_DICT['nose'],:2] - (body.landmarks[KEYPOINT_DICT['left_hip'],:2]+body.landmarks[KEYPOINT_DICT['right_hip'],:2])/2)
+        print("Estimated angle upper body:" + str(upper_body_angle_with_x))
     except:
         print("No upperbody landmarks detected")
     try:
-        lower_body_angle_with_y = angle_with_y((body.landmarks[KEYPOINT_DICT['left_hip'],:2]+body.landmarks[KEYPOINT_DICT['right_heel'],:2])/2 - (body.landmarks[KEYPOINT_DICT['left_hip'],:2]+body.landmarks[KEYPOINT_DICT['right_heel'],:2])/2)
+        lower_body_angle_with_x = angle_with_x((body.landmarks[KEYPOINT_DICT['left_hip'],:2]+body.landmarks[KEYPOINT_DICT['right_hip'],:2])/2 - (body.landmarks[KEYPOINT_DICT['left_heel'],:2]+body.landmarks[KEYPOINT_DICT['right_heel'],:2])/2)
+        print("Estimated angle lower body:" + str(lower_body_angle_with_x))
     except:
         print("No lowerbody landmarks detected")
 
-    average_body_angle = (upper_body_angle_with_y + lower_body_angle_with_y) / 2
+    average_body_angle = (abs(upper_body_angle_with_x)) #+ abs(lower_body_angle_with_x)) / 2 # bij een kleine verschuiving over naar onder x-as wordt ineens een bijna 360° overgang
 
     fall = False
-    #if len(body_position_array) >= 30:
-        #fall = person_has_fallen(average_body_angle)
+    if len(body_position_array) >= max_length_of_array and (not testing):
+        fall = person_has_fallen(average_body_angle)
+    else:
+        fall = person_has_fallen(average_body_angle)
 
-    return person_has_fallen(average_body_angle)
-
-# The argparse stuff has been removed to keep only the important code
+    return fall
 
 def init_tracker():
     return BlazeposeDepthai(lm_model="lite",
@@ -75,31 +94,18 @@ def init_renderer(tracker):
                     show_3d="mixed",
                     output="test.avi")
 
-def get_total_video_lentgh(video_file_name):
-    #https://stackoverflow.com/questions/3844430/how-to-get-the-duration-of-a-video-in-python
-    import cv2
-    video = cv2.VideoCapture(video_file_name)
-
-    duration = video.get(cv2.CAP_PROP_POS_MSEC)
-
-    return duration
-
-def trim_last_10_sec_from_video_file(video_file_name):
-    input = ffmpeg.input(video_file_name)
-    end_time = get_total_video_lentgh(video_file_name)
-    start_time = end_time - 10
-    trimmed = ffmpeg.trim(input, start=start_time,end=end_time)
-    out = ffmpeg.output(trimmed, video_file_name)
-
-def trim_and_send(video_file_name):
-    trim_last_10_sec_from_video_file(video_file_name)
-    # Send de video file code komt hier
-
 def run():
     fall_detected = False
     tracker = init_tracker()
     renderer = init_renderer(tracker)
+    old_time = time.time_ns() / 1000000000
+
     while not fall_detected:
+        new_time = time.time_ns()/1000000000
+        time_diff = new_time-old_time
+        old_time = new_time
+        # print("Delay per cycle: "+str(time_diff))
+
         # Run blazepose on next frame
         frame, body = tracker.next_frame()
         if frame is None: break
@@ -114,22 +120,6 @@ def run():
             else:
                 letter = 'F'
             cv2.putText(frame, letter, (frame.shape[1] // 2, 100), cv2.FONT_HERSHEY_PLAIN, 5, (0, 190, 255), 3)
-            '''if fall_detected: # Dit zou normaal de opname moeten stoppen en een nieuwe moeten maken
-                #body_position_array = []
-                #renderer.exit()
-                #tracker.exit()
-                new_file_name = "detection_"+str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))+".avi"
-                try:
-                    os.rename("test.avi",new_file_name) #Dit zou normaal de laatste nieuwe detectie moeten geven met tijd en datum
-                except:
-                    print("Error while trying to rename file")
-
-                #thread = threading.Thread(target=trim_and_send, args=(new_file_name))
-                #thread.start()
-                #thread.join()
-
-                #renderer = init_renderer()'''
-                #Hier moet de file nog bijgeknipt (laatste 10 sec) en verstuurd worden in een thread
         key = renderer.waitKey(delay=1)
 
 
