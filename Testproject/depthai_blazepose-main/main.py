bceeb75ce6cfb29e2e74e3c2326bb6843e7d14c1
ismaelwarnants <ismael.warnants@gmail.com>
2022-05-08 15:30:54 +0200

    Trimming last 10 sec added

--- Testproject/depthai_blazepose-main/main.py
+++ Testproject/depthai_blazepose-main/main.py
@@ -1,33 +1,52 @@
-import live_fall_test, os, datetime
+import fall_detection, os, datetime, time, ffmpeg
 
 
 def run():
-    live_fall_test.run()
-    rename_file()
+    fall_detection.run()
 
-def rename_file():
     new_file_name = "detection_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".avi"
+    rename_file(new_file_name)
+    trim_and_send(new_file_name)
+
+def rename_file(new_file_name):
+
     try:
-        os.rename("test.avi",
-                  new_file_name)  # Dit zou normaal de laatste nieuwe detectie moeten geven met tijd en datum
-    except:
-        print("Error while trying to rename file")
+        os.rename("test.avi", new_file_name)  # Dit zou normaal de laatste nieuwe detectie moeten geven met tijd en datum
+    except Exception as e:
+        print("Error while trying to rename file: "+str(e))
+
+
 
 def get_total_video_lentgh(video_file_name):
     #https://stackoverflow.com/questions/3844430/how-to-get-the-duration-of-a-video-in-python
-    import cv2
-    video = cv2.VideoCapture(video_file_name)
+    import subprocess, json
 
-    duration = video.get(cv2.CAP_PROP_POS_MSEC)
+    result = subprocess.check_output(
+        f'ffprobe -show_streams -select_streams v:0 -of json "{video_file_name}"',
+        shell=True).decode()
+    fields = json.loads(result)['streams'][0]
 
-    return duration
+    duration = fields['duration']
+    return float(duration)
 
 def trim_last_10_sec_from_video_file(video_file_name):
-    input = ffmpeg.input(video_file_name)
-    end_time = get_total_video_lentgh(video_file_name)
-    start_time = end_time - 10
-    trimmed = ffmpeg.trim(input, start=start_time,end=end_time)
-    out = ffmpeg.output(trimmed, video_file_name)
+    duration = 0
+    try:
+        duration = get_total_video_lentgh(video_file_name)
+        print("Total video length: " + str(duration))
+    except Exception as e:
+        print("Could not determine video length: " + str(e))
+
+    if duration > 10:
+        print("Video too long, clipping video...")
+        end_time = duration + 1
+        start_time = duration - 10
+        os.system("ffmpeg -i " + str(video_file_name) + " -ss  " + str(start_time) + " -to " + str(end_time) + " -c copy " + "Clipped_" + str(video_file_name))
+
+        '''input = ffmpeg.input(video_file_name)
+        
+        trimmed = ffmpeg.trim(input, start=start_time,end=end_time)
+        out = ffmpeg.output(trimmed, video_file_name)'''
 
 def trim_and_send(video_file_name):
     trim_last_10_sec_from_video_file(video_file_name)
@@ -42,5 +61,10 @@ def finish():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    try:
+        os.remove("test.avi") # In case an old video file was still present, it will be deleted before continuing
+    except:
+        print("No leftover file to remove, continuing...")
+
     while True:
         run()
