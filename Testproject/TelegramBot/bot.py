d03ff1c5c7afa70ab8562feb6cbeda09965bf91b
ismaelwarnants <ismael.warnants@gmail.com>
2022-05-10 19:39:26 +0200

    Sources added

--- Testproject/TelegramBot/bot.py
+++ Testproject/TelegramBot/bot.py
@@ -1,5 +1,8 @@
 import telegram
 
+# Docs: https://python-telegram-bot.readthedocs.io/en/stable/telegram.bot.html
+# Source: https://usp-python.github.io/06-bot/#sending-a-message-part-1
+
 api_key = "5347368126:AAGs3plNlDPqF9i057CDpT3rVLXv5AQFlSA"
 user_id = "619833245"
 
@@ -9,6 +12,7 @@ def send_message(message):
     bot.send_message(chat_id=user_id, text=message)
 
 def send_video(video_path):
+    # Source: https://stackoverflow.com/questions/47615956/send-video-through-telegram-python-api
     bot.send_video(chat_id=user_id, video=open(video_path, 'rb'), supports_streaming=True)
 
 def main():
